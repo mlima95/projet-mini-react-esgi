eca45ef4fdaaf4bd61fa2f924b5dcd5b2f53756e
edouardgachet <edouard.gachet@hetic.net>
2021-01-20 18:06:21 +0100

    [MiniReact] - add loop in method render

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 let ReactDom = {
     render: function (pElement, cElement) {
-        pElement.appendChild(cElement)
+        cElement.forEach(element => {
+            pElement.appendChild(element) 
+        });
     }
 }
 
 let createComponent = {
     createElem: function(type) {
+
         let myElement = document.createElement(type);
         let newContent = document.createTextNode('test');
         myElement.appendChild(newContent);
@@ -14,5 +17,8 @@ let createComponent = {
 }
 
 ReactDom.render(
-        document.getElementById('root'), createComponent.createElem('div')
+        document.getElementById('root'), [
+            createComponent.createElem('div'), 
+            createComponent.createElem('div'),
+        ]
 )
