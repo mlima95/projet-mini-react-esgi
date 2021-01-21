b55cee758f80fd3c6d30439a895a7fbafc2e5165
edouardgachet <edouard.gachet@hetic.net>
2021-01-21 15:01:21 +0100

    [MiniReact] - generate arbre

--- main.js
+++ main.js
@@ -6,15 +6,27 @@ let ReactDom = {
     }
 }
 
-let createComponent = {
-    createElem: function(type, attrbs, content) {
+let createElement = {
+    createElem: function(type, attrbs, content, childElement) {
 
         let myElement = document.createElement(type);
         for (const property in attrbs) {
-            console.log(property);
-            console.log(attrbs[property]);
             myElement.setAttribute(property.toString(), attrbs[property].toString())
         }
+
+        if(childElement != null && childElement != ''){
+            if(childElement.length > 1){
+                childElement.forEach(element => {
+                    myElement.appendChild(element)
+                });
+            }
+            else{
+                myElement.appendChild(childElement);
+            }
+        }else{
+            console.log('null')
+        }
+
         let newContent = document.createTextNode(content);
         myElement.appendChild(newContent);
         return myElement;
@@ -23,7 +35,18 @@ let createComponent = {
 
 ReactDom.render(
         document.getElementById('root'), [
-            createComponent.createElem('div', {'id': '1', 'class': 'test'}, 'content'), 
-            createComponent.createElem('div'),
+            createElement.createElem('div', {'id': '1', 'class': 'test'}, 'content', 
+                createElement.createElem('div', {'id': '2', 'class': 'test'}, 'content 2',
+                    createElement.createElem('ul', {'id': '2', 'class': 'test'}, 'content 3', 
+                        [
+                            createElement.createElem('li', {'id': '2', 'class': 'test'}, 'content 3'),
+                            createElement.createElem('li', {'id': '2', 'class': 'test'}, 'content 3'),
+                            createElement.createElem('li', {'id': '2', 'class': 'test'}, 'content 3')
+                        ]
+                    )
+                )
+            ), 
+            createElement.createElem('div', {'id': '2', 'class': 'test'}, 'content 2'),
+            createElement.createElem('a', {'id': '3', 'class': 'test', 'href': 'https://developer.mozilla.org/fr/docs/Web/API/Element/setAttribute'}, 'content 3'),
         ]
 )
