c669f3fe74a67bcee227b89d8e5578bbe20db783
Thomas Lemoine <contact@thomaslemoine.net>
2021-02-11 21:18:40 +0100

    style(blog): add subnav

--- src/Views/Blog.js
+++ src/Views/Blog.js
@@ -1,10 +1,15 @@
 import {React} from '../Modules/React.js';
 import {LoadPosts} from "../Component/LoadPosts.js";
 import {Header} from "../Component/Header.js";
+import {SubNav} from "../Component/SubNav.js";
 
 export default [
     React.createEl(
         Header,
+        undefined,
+    ),
+    React.createEl(
+        SubNav,
         undefined,
     ),
     React.createEl(
